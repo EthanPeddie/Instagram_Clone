e95dd51f4b25f8d19b4c99949ae60a8025fbf94e
EthanPeddie <kaungmyatthu2620@gmail.com>
2024-03-30 13:26:00 +0630

    fix bugs for props

--- src/components/FeedPost/FeedPost.tsx
+++ src/components/FeedPost/FeedPost.tsx
@@ -9,89 +9,67 @@ import {styles} from './styles';
 const FeedPost = () => {
   return (
     <View>
-      <Header />
-      <Content />
-      <Footer />
+      <View style={styles.headerContainer}>
+        <Image
+          style={styles.headerImage}
+          source={{
+            uri: 'https://metricool.com/wp-content/uploads/Screen-Shot-2023-06-28-at-2.21.12-PM.png',
+          }}
+        />
+        <Text style={styles.headerText}>Kaung Myat Thu</Text>
+        <View style={styles.headerIcon}>
+          <Entypo name="dots-three-horizontal" size={20} />
+        </View>
+      </View>
+      <View>
+        <Image
+          style={styles.contentImage}
+          source={{
+            uri: 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/09/instagram-image-size.jpg',
+          }}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <View style={styles.footerIcons}>
+          <AntDesign name="hearto" size={25} style={styles.footerIcon} />
+          <Ionicons
+            name="chatbubble-outline"
+            size={25}
+            style={styles.footerIcon}
+          />
+          <Feather name="send" size={25} style={styles.footerIcon} />
+          <Feather
+            name="bookmark"
+            size={25}
+            style={[styles.footerIcon, {marginLeft: 'auto'}]}
+          />
+        </View>
+        <View>
+          <Text style={styles.text}>
+            Likes by <Text style={styles.bold}>Ethan Peddie</Text> and{' '}
+            <Text style={styles.bold}>123 others</Text>
+          </Text>
+        </View>
+        <Text style={styles.text}>
+          <Text style={styles.bold}>Kaung Myat Thu</Text> Lorem ipsum dolor, sit
+          amet consectetur adipisicing elit. Totam eum temporibus, accusamus
+          ipsa ducimus adipisci numquam id voluptatibus et veniam cupiditate
+          quasi aliquam quidem ex dolorum animi porro ipsam fugiat.
+        </Text>
+        <View>
+          <Text>View All 18 comments</Text>
+          <View style={styles.comment}>
+            <Text style={styles.commentText}>
+              <Text style={styles.bold}>Shwe War</Text> Lorem ipsum dolor sit
+              amet consectetur adipisicing elit.
+            </Text>
+            <AntDesign name="hearto" size={16} />
+          </View>
+        </View>
+        <Text>20 January 2024</Text>
+      </View>
     </View>
   );
 };
-
-const Header = () => (
-  <View style={styles.headerContainer}>
-    <Image
-      style={styles.headerImage}
-      source={{
-        uri: 'https://metricool.com/wp-content/uploads/Screen-Shot-2023-06-28-at-2.21.12-PM.png',
-      }}
-    />
-    <Text style={styles.headerText}>Kaung Myat Thu</Text>
-    <View style={styles.headerIcon}>
-      <Entypo name="dots-three-horizontal" size={20} />
-    </View>
-  </View>
-);
-
-const Content = () => (
-  <View>
-    <Image
-      style={styles.contentImage}
-      source={{
-        uri: 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/09/instagram-image-size.jpg',
-      }}
-    />
-  </View>
-);
-
-const Footer = () => (
-  <View style={styles.footerContainer}>
-    <View style={styles.footerIcons}>
-      <AntDesign name="hearto" size={25} style={styles.footerIcon} />
-      <Ionicons name="chatbubble-outline" size={25} style={styles.footerIcon} />
-      <Feather name="send" size={25} style={styles.footerIcon} />
-      <Feather
-        name="bookmark"
-        size={25}
-        style={[styles.footerIcon, {marginLeft: 'auto'}]}
-      />
-    </View>
-    <Likes />
-    <PostDescription />
-    <Comment />
-    <PostedDate />
-  </View>
-);
-
-const Likes = () => (
-  <View>
-    <Text style={styles.text}>
-      Likes by <Text style={styles.bold}>Ethan Peddie</Text> and{' '}
-      <Text style={styles.bold}>123 others</Text>
-    </Text>
-  </View>
-);
-
-const PostDescription = () => (
-  <Text style={styles.text}>
-    <Text style={styles.bold}>Kaung Myat Thu</Text> Lorem ipsum dolor, sit amet
-    consectetur adipisicing elit. Totam eum temporibus, accusamus ipsa ducimus
-    adipisci numquam id voluptatibus et veniam cupiditate quasi aliquam quidem
-    ex dolorum animi porro ipsam fugiat.
-  </Text>
-);
-
-const Comment = () => (
-  <View>
-    <Text>View All 18 comments</Text>
-    <View style={styles.comment}>
-      <Text style={styles.commentText}>
-        <Text style={styles.bold}>Shwe War</Text> Lorem ipsum dolor sit amet
-        consectetur adipisicing elit.
-      </Text>
-      <AntDesign name="hearto" size={16} />
-    </View>
-  </View>
-);
-
-const PostedDate = () => <Text>20 January 2024</Text>;
 
 export default FeedPost;
