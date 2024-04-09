1a6cd28b2d7f3474f6c04fc33339151da851e019
EthanPeddie <kaungmyatthu2620@gmail.com>
2024-04-09 19:33:26 +0630

    Connect our app to the backend with AWS Amplify

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navigation from './src/navigation';
+import {Amplify} from 'aws-amplify';
+import config from './src/aws-exports';
 
+Amplify.configure(config);
 const App = () => {
   return <Navigation />;
 };
